46dd256dfd1e64fb84b9c2d0e5c645c236e1fe8f
supriya <supriyaramesh28@gmail.com>
2025-08-11 10:08:04 +0200

    First version of the project

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spaipur- <spaipur-@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/27 11:16:41 by spaipur-          #+#    #+#             */
+/*   Updated: 2025/08/04 20:06:42 by spaipur-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#ifndef BUFFERSIZE
+# define BUFFER_SIZE 10
+#endif
+
+#include <unistd.h>
+#include <stdlib.h>
+
+char *get_next_line(int fd);
+char *ft_next_line(char *buffer);
+char *ft_strjoin(char *s1,const char *s2);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spaipur- <spaipur-@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/27 11:22:19 by spaipur-          #+#    #+#             */
+/*   Updated: 2025/08/11 09:28:22 by spaipur-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t			i;
+	unsigned char	*str;
+
+	i = 0;
+	str = (unsigned char *)s;
+	while (i < n)
+	{
+		str[i] = '\0';
+		i++;
+	}
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	char	*s;
+
+	s = malloc(count * size);
+	if (!s)
+		return (NULL);
+	ft_bzero(s, count * size);
+	return (s);
+}
+
+int ft_strlen(const char *s)
+{
+	int len;
+	
+	len = 0;
+	while(*s++)
+		len++;
+	return (len);
+}
+
+char *ft_strjoin(char *s1,const char *s2)
+{
+	char *str;
+	char *ptr;
+	
+	if(!s1 || !s2)
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	ptr = str;
+	while(s1)
+		*ptr++ = *s1++;
+	while(s2)
+		*ptr++ = *s2++;
+	*ptr++ = '\0';
+	if (s1)
+		free(s1);
+	return (str);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned char	ch;
+
+	ch = (unsigned char)c;
+	while (*s)
+	{
+		if ((unsigned char) *s == ch)
+			return ((char *)s);
+		s++;
+	}
+	if (ch == '\0')
+		return ((char *)s);
+	return (NULL);
+}
